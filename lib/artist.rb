8a8c6ba940af9fb38bc85564ff9d21df0e497a93
Abraham <abdiaz2018@gmail.com>
2021-02-05 17:40:39 -0800

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
     @@all = []
-    attr_accessor :name :songs
+    attr_accessor :name, :artist, :song
     def initialize(name)
         @name = name
         @@all << self
@@ -9,6 +9,22 @@ class Artist
         @@all
     end
     def songs
-        
+        Song.all.select {|song| song.artist == self}
+    end
+    def add_song(song)
+        song.artist = self
+    end
+    def self.find_or_create_by_name(artist_name)
+        find_artist = self.all.find {|artist_object| artist_object.name == artist_name}
+        if find_artist
+            find_artist
+        else
+            Artist.new(artist_name)
+        end
+    end
+    def print_songs
+        self.songs.each do |song_object|
+            puts song_object.name
+        end
     end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -8,4 +8,13 @@ class Song
     def self.all
         @@all
     end
+    def self.new_by_filename(filename)
+        split_array = filename.split(" - ")
+        song_1 = Song.new(split_array[1])
+        song_1.artist_name=(split_array[0])
+        song_1
+    end
+    def artist_name=(name)
+        self.artist = Artist.find_or_create_by_name(name)
+    end
 end
